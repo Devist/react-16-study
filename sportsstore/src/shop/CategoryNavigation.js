e4f82ecdd09a29d9a31d7df441c4c8815b1d6e76
Devist <aboutdh89@naver.com>
2020-11-09 17:48:13 +0900

    Study chapter 8 - add authentication

--- sportsstore/src/shop/CategoryNavigation.js
+++ sportsstore/src/shop/CategoryNavigation.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 // import { Link } from "react-router-dom"
 import { ToggleLink } from "../ToggleLink"
+import { Link } from "react-router-dom"
 
 export class CategoryNavigation extends Component {
   render() {
@@ -18,6 +19,12 @@ export class CategoryNavigation extends Component {
               {cat}
             </ToggleLink>
           ))}
+        <Link
+          className="btn btn-block btn-secondary fixed-bottom m-2 col-3"
+          to="/admin"
+        >
+          Administration
+        </Link>
       </React.Fragment>
     )
   }
