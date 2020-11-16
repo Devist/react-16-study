f99d26a9d7ce1641b8d8ae7d6bafeb0bcf6c4e78
Devist <aboutdh89@naver.com>
2020-11-16 10:42:43 +0900

    Study chapter 10 - batch delivery of props and default props

--- components/src/CallbackButton.js
+++ components/src/CallbackButton.js
@@ -1,12 +1,18 @@
 import React from "react"
+import { SimpleButton } from "./SimpleButton"
 
 export function CallbackButton(props) {
+  let { theme, ...childProps } = props
+
   return (
-    <button
+    <SimpleButton
+      {...childProps}
       className={`btn btn-${props.theme} btn-sm m-1`}
-      onClick={props.callback}
-    >
-      {props.text}
-    </button>
+    />
   )
+}
+
+CallbackButton.defaultProps = {
+  text: "Default Text",
+  theme: "warning"
 }
