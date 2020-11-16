f88e9a435c3f4e4fe29dfa92acaa723847bc646c
Devist <aboutdh89@naver.com>
2020-11-16 09:10:59 +0900

    Study chapter 9 - Public folder for static content

---
+++ projecttools/src/App.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React, { Component } from "react"
+import "./App.css"
+import reactLogo from "./logo.svg"
+
+let name = "Adam"
+const city = "London"
+
+export default class extends Component {
+  message = () => `Hello ${name} from ${city}`
+
+  render = () => (
+    <div className="text-center">
+      <h4 className="bg-primary text-white text-center p-3">
+        {this.message()}
+      </h4>
+      <img src={reactLogo} alt="reactLogo" />
+      <link rel="stylesheet" href={process.env.PUBLIC_URL + "/static.css"} />
+      <img src={process.env.PUBLIC_URL + "/logo.svg"} alt="reactLogo" />
+    </div>
+  )
+}
